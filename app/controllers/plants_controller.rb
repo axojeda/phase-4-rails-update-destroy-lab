02c23a86692817d6ad6e8a55a8f7e6158ee3d430
Aura <ax.pink@gmail.com>
2023-02-22 11:36:25 -0500

    commit tree c6c6a123fbcdae67f63a4125e9d936e5bd9b621f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+   plants = Plant.find_by(id: params[:id])
+   plant.update(plant_params)
+   render json: plant, only: [:id, :image, :price, :is_in_stock]
+
+  end
+
+  def destroy
+    plant = Plant.find_by(:id params[:id])
+    plant.destroy
+    #render json: {}, status: :no_content
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+end
 end
